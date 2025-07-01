dd2d1935d071e171d4e575d01f4b9c53a2de4745
LiaoZiqi-GZFLS <503546675@qq.com>
2025-07-01 16:35:38 +0800

    push

--- try0/Quantum-pro.cpp
+++ try0/Quantum-pro.cpp
@@ -78,8 +78,9 @@ public:
             data[0][0] = 1; data[0][1] = 0;
             data[1][0] = 0; data[1][1] = 1;
         } else if (c == 'H') {
-            data[0][0] = 1 / std::sqrt(2); data[0][1] = 1 / std::sqrt(2);
-            data[1][0] = 1 / std::sqrt(2); data[1][1] = -1 / std::sqrt(2);
+            data[0][0] = 1; data[0][1] = 1;
+            data[1][0] = 1; data[1][1] = -1;
+            num++;
         } else if (c == 'X') {
             data[0][0] = 0; data[0][1] = 1;
             data[1][0] = 1; data[1][1] = 0;
@@ -103,7 +104,11 @@ public:
         data[i][j] = value;
     }
 
-    Matrix operator+(const Matrix& other) const {
+    int getNum() const {
+        return num;
+    }
+
+    /*Matrix operator+(const Matrix& other) const {
         Matrix result;
         for (int i = 0; i < 2; ++i) {
             for (int j = 0; j < 2; ++j) {
@@ -111,16 +116,27 @@ public:
             }
         }
         return result;
-    }
+    }*/
 
     Matrix operator*(const Matrix& other) const {
         Matrix result;
+        result.num = this->num + other.num;
         for(int i = 0; i < 2; i++) {
             for(int k = 0; k < 2; k++) {
                 for (int j = 0; j < 2; j++) {
                     result.set(i, j, result.get(i, j) + this->get(i, k) * other.get(k, j));
+                    
                 }
             }
+        }
+        if(result.num>=2){
+            std::complex<double> c2(2, 0);
+            for(int i = 0; i < 2; i++) {
+                for(int j = 0; j < 2; j++) {
+                    result.set(i, j, result.get(i, j) / c2);
+                }
+            }
+            result.num -= 2;
         }
         return result;
     }
@@ -170,6 +186,17 @@ void simulate(size_t N, const char* Gates, std::complex<double>& Alpha, std::com
     }
     Alpha = result.get(0, 0);
     Beta = result.get(1, 0);
+    if(result.getNum() != 0) {
+        for(int i = 0; i < result.getNum()/2; i++) {
+            Alpha /= 2;
+            Beta /= 2;
+        }
+        if(result.getNum() % 2 == 1) {
+            std::complex<double> c2(std::sqrt(2.0), 0);
+            Alpha /= c2;
+            Beta /= c2;
+        }
+    }
 
     // 归一化量子态
     double norm = std::sqrt(std::abs(Alpha * std::conj(Alpha) + Beta * std::conj(Beta)));
